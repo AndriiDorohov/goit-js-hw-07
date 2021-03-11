7d2af814d2758a0fdcb07c6de5a40e17c475b9ac
Andrii Dorohov <wedpositive@gmail.com>
2021-03-11 19:54:00 +0200

    1

--- js/task-05.js
+++ js/task-05.js
@@ -5,3 +5,9 @@
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
 
+let input = document.querySelector('#name-input');
+
+
+input.oninput = function () {
+    document.getElementById('name-output').innerHTML = input.value;
+  };
